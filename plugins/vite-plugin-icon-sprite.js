da5ea07089be60693d6db1a9f0ef7ebbe6015e39
Artem Cherniavsky <cherniavsky.frontend@gmail.com>
2025-02-08 01:11:15 +0200

    transfer sprite icons

--- plugins/vite-plugin-icon-sprite.js
+++ plugins/vite-plugin-icon-sprite.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default function IconSpritePlugin() {
   async function generateIconSprite() {
     // Read the SVG files in the static/icons folder
-    const iconsDir = path.join(process.cwd(), 'public', 'icons');
+    const iconsDir = path.join(process.cwd(), 'src', 'assets', 'img', 'icons');
     const files = await fs.readdir(iconsDir);
     let symbols = '';
 
@@ -22,7 +22,7 @@ export default function IconSpritePlugin() {
 
     // Write the SVG sprite to a file in the static folder
     const sprite = `<svg width="0" height="0" style="display: none">\n\n${symbols}</svg>`;
-    await fs.writeFile(path.join(process.cwd(), 'public', 'icon-sprite.svg'), sprite);
+    await fs.writeFile(path.join(process.cwd(), 'src', 'assets', 'img', 'icon-sprite.svg'), sprite);
   }
 
   return {
@@ -34,7 +34,7 @@ export default function IconSpritePlugin() {
 
     configureServer(server) {
       // Regenerate during development whenever an icon is added
-      server.watcher.add(path.join(process.cwd(), 'static', 'icons', '*.svg'));
+      server.watcher.add(path.join(process.cwd(), 'src', 'assets', 'img', 'icons', '*.svg'));
       server.watcher.on('change', async (changedPath) => {
         if (changedPath.endsWith('.svg')) return generateIconSprite();
       });
